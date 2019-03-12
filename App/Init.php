3c1984d0a6733c80b88732b70fbc46787da7910e
Alessandro Ferreira Pereira <datacrash.job@gmail.com>
2019-03-11 23:26:47 -0300

    Criando Rotas em Init e referenciando Controllers

---
+++ App/Init.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+class Init
+{
+    private $routes;
+
+    public function __construct()
+    {
+        $this->initRoutes();
+        $this->run($this->getURL());
+    }
+
+    public function initRoutes()
+    {
+        $ar['home'] = array('route'=>'/', 'controller'=>'index', 'action'=>'index');
+        $ar['empresa'] = array('route'=>'/empresa', 'controller'=>'index', 'action'=>'empresa');
+        $this->setRoutes($ar);
+    }
+
+    public function run($url)
+    {
+        array_walk($this->routes, function($route) use($url)
+        {
+            if($url == $route['route'])
+            {
+                $class = "App\\Controllers\\".ucfirst($route['controller']);
+                $action = $route['action'];
+                $controller = new $class;
+                $controller->$action();
+            }
+        });
+    }
+
+    public function setRoutes(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function getURL()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+
+}
