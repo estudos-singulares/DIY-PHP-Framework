a5cac89d0a750321f86eb9b718e89d1720605006
Alessandro Ferreira Pereira <datacrash.job@gmail.com>
2019-03-13 03:30:23 -0300

    Usando Layout nas Views

--- Vendor/DIY/Controller/Action.php
+++ Vendor/DIY/Controller/Action.php
@@ -5,16 +5,27 @@ namespace DIY\Controller;
 class Action
 {
     protected $view;
+    protected $action;
 
-	public function __construct()
-	{
+    public function __construct()
+    {
         $this->view = new \stdClass;
     }
 
-	public function render($action)
-	{
+    public function render($action, $layout = true)
+    {
+        $this->action = $action;
+        if ($layout == true && file_exists('../App/Views/layout.phtml')) {
+            include_once '../App/Views/layout.phtml';
+        } else {
+            $this->content();
+        }
+    }
+
+    public function content()
+    {
         $atual = get_class($this);
         $singleClassName = strtolower(str_replace("App\\Controllers\\", "", $atual));
-        include_once '../App/Views/'.$singleClassName.'/'.$action.'.phtml';
-	}
+        include_once '../App/Views/' . $singleClassName . '/' . $this->action . '.phtml';
+    }
 }
