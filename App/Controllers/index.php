73adb41e654c9d5241978899211c68657ebe3918
Alessandro Ferreira Pereira <datacrash.job@gmail.com>
2019-03-13 04:08:21 -0300

    Refatorando Controller para Injeção de Dependência

--- App/Controllers/index.php
+++ App/Controllers/index.php
@@ -2,14 +2,14 @@
 
 namespace App\Controllers;
 
-use App\Models\Artigo;
 use DIY\Controller\Action;
+use \DIY\DI\Container;
 
 class Index extends Action
 {
     public function index()
     {
-        $artigo = new Artigo(\App\Init::getDB());
+        $artigo = Container::getClass("Artigo");
         $artigos = $artigo->fetchAll();
         $this->view->artigos = $artigos;
         $this->render('index');
